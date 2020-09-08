31d526b70557dacf7b019b81f41757b15b2e4648
kyuri <kyuri@mail.tmaxcloudspace.com>
2020-09-08 05:01:18 +0000

    finished the course

---
+++ movie_app/src/routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About(props) {
+    console.log(props);
+    return (
+        <div className="about__containter">
+            <span>About this page: i built it beacuase i love movies</span>
+            <span>About this page: i built it beacuase i love movies</span>
+        </div>
+
+    )
+
+}
+
+export default About;
